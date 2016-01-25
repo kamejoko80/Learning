2812da4505734ff722549f7e2bb281c7b81daebd
HopingTang <wan52390010@gmail.com>
2016-01-25 13:16:55 +0800

    learn python by reading byte of python chapter one to eight.

---
+++ WebProgramming/python/functions/func_return.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+# Filename: func_return.py
+
+def maximum(x, y):
+    if x > y:
+        return x
+    else:
+        return y
+
+print maximum(2, 3)
+
+def someFunction():
+    pass
+
+print someFunction()
+
+
